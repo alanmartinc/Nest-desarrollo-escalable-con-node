28dcb36ccfb85166be49eb6bc3fc1d1ed160a4a2
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2022-12-07 02:12:05 -0300

    preparacion del proyecto

---
+++ 01-TypeScript-intro/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import { setupCounter } from './counter'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
