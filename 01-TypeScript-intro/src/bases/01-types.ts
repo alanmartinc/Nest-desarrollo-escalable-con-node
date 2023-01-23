6a8b315249575f79515e115df6eb51b308afe3c9
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-22 21:00:01 -0300

    tipos de datos continuacion

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,5 +1,11 @@
-export const name = 'Alan';
-export const age: number = 26;
+export let name = 'Alan';
+export const age: number = 25;
 export const isValid: boolean = true;
+export const templateString = `
+    Esto es un string multilinea
+`;
+
+name = "Nancy";
 
 console.log({isValid});
+console.log({templateString});
