75c83f5f9dd7efc6569b64a2bbdcb17a5689f64e
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-22 23:08:14 -0300

    getters, metodos y this

--- 01-TypeScript-intro/src/bases/03-clases.ts
+++ 01-TypeScript-intro/src/bases/03-clases.ts
@@ -1,10 +1,27 @@
 export class Pokemon {
+    // GETTERS
+    get imageUrl(): string {
+        return `https://pokemon.com/${this.id}.jpg`
+    }
+
     constructor(
         public readonly id: number, 
         public name: string
     ) {}
+
+    // METODOS
+    scream() {
+        console.log(`${this.name.toUpperCase()} !!!`);
+    }
+
+    speak() {
+        console.log(`${this.name}, ${this.name}`);
+    }
 }
 
 export const charmander = new Pokemon(1, 'Charmander'); 
 
-charmander.name = 'Mew'; 
+charmander.name = 'Mew';
+
+charmander.scream();
+charmander.speak();
