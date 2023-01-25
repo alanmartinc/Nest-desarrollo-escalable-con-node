69243cf43811c046f8267a992dddc08695cd2f19
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-24 23:50:24 -0300

    obtener un carro por id

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
+// Escuchan la solicitud y emiten una respuesta
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Jeep'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    return this.cars[id];
   }
 }
