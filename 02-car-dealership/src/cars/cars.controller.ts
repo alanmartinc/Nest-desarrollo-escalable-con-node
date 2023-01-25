c778e37c8e33e60fdb32023fe4fb00b5571b3a41
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-25 00:13:32 -0300

    inyeccion de dependencias

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 // Escuchan la solicitud y emiten una respuesta
 @Controller('cars')
 export class CarsController {
+  // Inyeccion de dependecias para usar los datos del servicio
+  constructor(private readonly CarsService: CarsService) {}
   @Get()
   getAllCars() {
-    return this.cars;
+    // Retorna todos los carros
+    return this.CarsService.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
-    return this.cars[id];
+    return this.CarsService.findOneById(+id);
   }
 }
--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -19,4 +19,13 @@ export class CarsService {
       model: 'Cherokee',
     },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    return car;
+  }
 }
