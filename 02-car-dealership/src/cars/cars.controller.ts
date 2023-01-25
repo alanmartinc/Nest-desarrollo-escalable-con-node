ea9e0ebb4f53efe744b9da66468c1766c0509d31
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-24 23:36:22 -0300

    desactivar prettier

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
-    @Get()
-    getAllCars() {
-        return ['Toyota', 'Honda', 'Jeep'];
-    }
+  @Get()
+  getAllCars() {
+    return ['Toyota', 'Honda', 'Jeep'];
+  }
 }
