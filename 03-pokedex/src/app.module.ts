8500edd328f1bff3c77e6f5bc058bf8bcf947bfe
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-29 23:13:40 -0300

    se agrega carpeta

---
+++ 03-pokedex/src/app.module.ts
@@ -0,0 +1,12 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+})
+export class AppModule {}
