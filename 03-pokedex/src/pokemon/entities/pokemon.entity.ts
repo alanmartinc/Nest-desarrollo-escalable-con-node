10004ae754fa208295943f117f1a1bbc776aa44c
Alan Martín Cabot <52534693+alanmartinc@users.noreply.github.com>
2023-01-30 23:28:48 -0300

    crear esquemas y modelos

--- 03-pokedex/src/pokemon/entities/pokemon.entity.ts
+++ 03-pokedex/src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,20 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// Esquemas y Modelos
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PokemonSchema,
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
