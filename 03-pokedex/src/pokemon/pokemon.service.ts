a3a228b02a96ee61c0c02cc7a970d08df5c76c4f
alanmartinc <alancabot.ac@gmail.com>
2024-02-05 20:22:10 +0100

    post - recibir y validar la data

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -5,7 +5,8 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 @Injectable()
 export class PokemonService {
   create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    return createPokemonDto;
   }
 
   findAll() {
