b021bba5e7759b2dc29c39bb804ede7f55e453ce
alanmartinc <alancabot.ac@gmail.com>
2024-02-05 09:48:10 -0600

    servir contenido estatico

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    })
+  ],
 })
 export class AppModule {}
