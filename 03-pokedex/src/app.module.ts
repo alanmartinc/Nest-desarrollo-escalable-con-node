74a2a624fdf88f84b04e44e036061696f90d9fd0
alanmartinc <alancabot.ac@gmail.com>
2024-02-05 10:43:51 -0600

    global prefix

--- 03-pokedex/src/app.module.ts
+++ 03-pokedex/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
-    })
+    }),
+    PokemonModule
   ],
 })
 export class AppModule {}
