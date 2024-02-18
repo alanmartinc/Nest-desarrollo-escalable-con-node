ad430abf6b974bb9d3efa8c834e3bc75dd5ad39e
alanmartinc <alancabot.ac@gmail.com>
2024-02-18 20:31:50 +0100

    realizar peticion http desde nest

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
-    return 'Seed executed';
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+    });
+    
+    return data.results;
   }
 }
