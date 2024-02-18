645c8bfc6e44e09c45dc2226794d5ac50ca573c0
alanmartinc <alancabot.ac@gmail.com>
2024-02-18 17:35:25 +0100

    validar y eliminar en una sola consulta

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -66,8 +66,11 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const result = this.pokemonModel.findByIdAndDelete(id);
-    return result;
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+    if(deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id "${id}" not found`);
+
+    return;
   }
 
   private handleExceptions(error: any) {
